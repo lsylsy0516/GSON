15ecfb96a0050e7167ffd06ccc064cfb64119442
lsylsy0516 <1419853406@qq.com>
2024-09-01 23:29:41 +0800

    first commit

---
+++ 2D_lidar_person_detection/dr_spaam_ros/scripts/node.py
@@ -0,0 +1,11 @@
+#!/home/luo/miniconda3/envs/percept/bin/python
+import rospy
+from dr_spaam_ros.dr_spaam_ros import DrSpaamROS
+
+if __name__ == '__main__':
+    rospy.init_node('dr_spaam_ros')
+    try:
+        DrSpaamROS()
+    except rospy.ROSInterruptException:
+        pass
+    rospy.spin()
